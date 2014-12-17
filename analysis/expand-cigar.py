149663eac080967269d7285dbb21f72c6b6842f2
Nick Loman <n.j.loman@bham.ac.uk>
2014-12-17 08:04:27 +0000

    arq5x update of expand-cigar.py

--- analysis/expand-cigar.py
+++ analysis/expand-cigar.py
@@ -1,22 +1,19 @@
-#!/usr/bin/python
-
-# from the legendary arq5x
-
 import sys
 import pysam
 import argparse
-from itertools import groupby
+
+"""Author: Aaron R. Quinlan, December 2014"""
 
 # http://pysam.readthedocs.org/en/latest/api.html#pysam.AlignedRead.cigar
-MATCH  = 0  # M
-INS    = 1  # I
-DEL    = 2  # D
-SKIP   = 3  # N
-SOFT   = 4  # S
-HARD   = 5  # H
-PAD    = 6  # P
-EQUAL  = 7  # =
-DIFF   = 8  # X
+MATCH = 0  # M
+INS = 1    # I
+DEL = 2    # D
+SKIP = 3   # N
+SOFT = 4   # S
+HARD = 5   # H
+PAD = 6    # P
+EQUAL = 7  # =
+DIFF = 8   # X
 
 
 def get_chrom(fasta_fh, chrom):
@@ -25,36 +22,28 @@ def get_chrom(fasta_fh, chrom):
     """
     return fasta_fh.fetch(chrom)
 
-def expand_match_op(qry_seq, ref_seq):
+
+def expand_match(qry_seq, ref_seq):
     """
     Return an expanded list of CIGAR ops
     based upon nuceltotide matches (=, or 7)
     and mismatches (X, or 8)
-
-    This onvert runs into RLE tuples (i.e. CIGAR ops/lengths)
-    for example, the following:
-    ref: ATTCAGGG
-    qry: ATTCAGAG
-    becomes (via _runs()):
-         ======X=
-    or:  77777787    
-    this is converted to:
-    [(7, 5), (8, 1), (7, 1)]
     """
-    def _runs(qry_seq, ref_seq):
-        """
-        Create runs of matches and mismatches
-        """
-        for idx, q_nucl in enumerate(qry_seq):
-            if q_nucl == ref_seq[idx]:
-                yield 7 #EQUAL (=)
-            else:
-                yield 8 #DIFF (X)
+    prev_op = None
+    curr_op = None
+    length = 1
+    for idx, q_nucl in enumerate(qry_seq):
+        if q_nucl == ref_seq[idx]: curr_op = 7  # EQUAL (=)
+        else: curr_op = 8  # DIFF (X)
 
-    cigar_ops = []
-    for k, v in groupby(_runs(qry_seq, ref_seq), lambda x: x):
-        cigar_ops.append((k,len(list(v))))
-    return cigar_ops
+        if curr_op == prev_op:
+            length += 1
+        elif prev_op is not None:
+            yield (prev_op, length)
+            length = 1
+        prev_op = curr_op
+    yield (curr_op, length)
+
 
 def main(args):
 
@@ -82,13 +71,13 @@ def main(args):
             op_len = cigar_tuple[1]
             if op == EQUAL or op == DIFF or op == MATCH:
                 if op == MATCH:
-                    qry_seq = read.query_sequence[qry_pos:qry_pos+op_len]
-                    ref_seq = curr_chrom_seq[ref_pos:ref_pos+op_len]
-                    
-                    # expand the M CIGAR op.
-                    new_cigar_ops = expand_match_op(qry_seq, ref_seq)
-                    for new_cigar_tuple in new_cigar_ops:
-                        new_cigar.append(new_cigar_tuple)
+                    qry_seq = read.query_sequence[qry_pos:qry_pos + op_len]
+                    ref_seq = curr_chrom_seq[ref_pos:ref_pos + op_len]
+                    if qry_seq == ref_seq:
+                        new_cigar.append((7, len(qry_seq)))  # EQUAL (=)
+                    else:  # expand the M CIGAR op into X and = ops.
+                        for new_cigar_tuple in expand_match(qry_seq, ref_seq):
+                            new_cigar.append(new_cigar_tuple)
                 else:
                     new_cigar.append(cigar_tuple)
                 ref_pos += op_len
@@ -103,24 +92,22 @@ def main(args):
                 new_cigar.append(cigar_tuple)
 
         # replace the old CIGAR and write updated record.
-        read.cigar = new_cigar
+        read.cigar=new_cigar
         outfile.write(read)
-        prev_chrom_id = curr_chrom_id
-
+        prev_chrom_id=curr_chrom_id
     outfile.close()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(prog='expand_cigar.py')
+    parser=argparse.ArgumentParser(prog='expand_cigar.py')
     parser.add_argument('--bam',
-    dest='bam',
-    metavar='STRING',
-    help='The sorted BAM file whose CIGARs you wish expand.')
+        dest='bam',
+        metavar='STRING',
+        help='The sorted BAM file whose CIGARs you wish expand.')
     parser.add_argument('--fasta',
-    dest='fasta',
-    metavar='STRING',
-    help='The reference genome used to create the BAM.')
-    # parse the args and call the selected function
-    args = parser.parse_args()
-    
+        dest='fasta',
+        metavar='STRING',
+        help='The reference genome used to create the BAM.')
+    args=parser.parse_args()
+
     main(args)
 
